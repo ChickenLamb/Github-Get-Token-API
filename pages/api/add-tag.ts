56ec467c68ed705483651f20364a10ee769334f9
ChickenLamb <vincentlin46@gmail.com>
2023-04-05 16:58:31 +0800

    add-tag API modify

--- pages/api/add-tag.ts
+++ pages/api/add-tag.ts
@@ -9,19 +9,26 @@ export default async function handler(
   res: NextApiResponse
 ) {
 
-  let config = {
-    method: 'post',
-    maxBodyLength: Infinity,
-    url: `${req.query.labels_url}`,
-    headers: { 
-        'Accept': 'application/vnd.github+json', 
-        'Cache-Control': 'no-store', 
-        'Authorization': 'Bearer '+`${req.query.token}`, 
-        'Content-Type': 'application/json', 
-      },
-      data : {labels:[`${req.query.tag}`]}
-    
-  };
+    let data = JSON.stringify({
+        "labels": [
+          "bug",
+          "enhancement"
+        ]
+      });
+      
+      let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.github.com/repos/ChickenLamb/B1/issues/1/labels',
+        headers: { 
+          'Accept': 'application/vnd.github+json', 
+          'Cache-Control': 'no-store', 
+          'Authorization': 'Bearer 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', 
+          'Content-Type': 'application/json', 
+          'Cookie': '_octo=GH1.1.1929348507.1679046023; logged_in=no'
+        },
+        data : data
+      };
   axios.request(config)
 .then((response) => {
   console.log('add tag successfully');
